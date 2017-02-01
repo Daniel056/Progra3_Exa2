9ffbcaf2bf8861abfdbdb80b37a8cdc845f6d3a2
zagad <zagad@192.168.0.102>
2017-02-01 16:47:17 -0600

    Capa de dao, pom, hibernateconfig, webpages

---
+++ ExamenParcial2/src/main/java/ac/cr/una/backend/dao/BookDAOImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.cr.una.backend.dao;
+
+import ac.cr.una.backend.model.Book;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author zagad
+ */
+public class BookDAOImpl implements BookDAO {
+
+    private final Session session = HibernateUtil.getSessionFactory().openSession();
+    
+    @Override
+    public boolean deleteAll() {
+        boolean isDeleted = false;
+        session.getSessionFactory().getCurrentSession().createQuery("delete from book").executeUpdate();
+        isDeleted=true;
+        return isDeleted;
+    }
+
+    @Override
+    public Book save(Book book) {
+        session.beginTransaction();
+        session.save(book);
+        session.getTransaction().commit();
+
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> bookList = new ArrayList();
+        
+        bookList = session.createCriteria(Book.class).list();
+
+        return bookList;
+    }
+    
+}
---
+++ ExamenParcial2/src/main/java/ac/cr/una/backend/dao/BookTypeDAOImpl.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.cr.una.backend.dao;
+
+
+import ac.cr.una.backend.model.BookType;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author zagad
+ */
+public class BookTypeDAOImpl implements BookTypeDAO {
+    
+    private final Session session = HibernateUtil.getSessionFactory().openSession();
+
+    @Override
+    public boolean deleteAll() {
+        boolean isDeleted = false;
+        session.getSessionFactory().getCurrentSession().createQuery("delete from booktype").executeUpdate();
+        isDeleted=true;
+        return isDeleted;
+    }
+
+    @Override
+    public BookType save(BookType bookType) {
+        session.beginTransaction();
+        session.save(bookType);
+        session.getTransaction().commit();
+
+        return bookType;
+    }
+
+    @Override
+    public BookType findByType(String type) {
+        BookType bookType = null;
+        Query query = session.createQuery("from booktype where type = :type ");
+        query.setParameter("type",type);
+
+        if (query.list().size() > 0) {
+            bookType = (BookType) query.list().get(0);
+        }
+
+        return bookType;
+    }
+    
+    
+}
